1484db82b2b63f24b59e8fe4b97c140b6cd50a18
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-09-01 22:20:38 +0530

    Update Solution.java

--- String Compression/Solution.java
+++ String Compression/Solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public int compress(char[] chars) {
-       int count=1;
-       int write=0;
-        for(int i=1; i <= chars.length; i++){
-            if(i < chars.length && chars[i] == chars[i-1]){
+        int count=1;
+        int index=0;
+        for(int i=1; i<=chars.length;i++){
+            while((i < chars.length) && (chars[i] == chars[i-1])){
                 count++;
-            }else{
-                chars[write++]=chars[i-1];
-                if(count > 1){
-                    char[] countChars = String.valueOf(count).toCharArray();
-                    for(char c: countChars){
-                        chars[write++]=c;
-                    }
+                i++;
+            }
+            chars[index++]=chars[i-1]; 
+            if(count > 1){
+                String s= String.valueOf(count);
+                for(char a: s.toCharArray()){
+                    chars[index++]=a;
                 }
                 count=1;
             }
         }
-        return write;
+        return index;
     }
 }
