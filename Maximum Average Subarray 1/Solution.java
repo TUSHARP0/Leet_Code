7b236cfdb6610009eba3a7f90334619f8d487cbd
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-09-12 09:26:10 +0530

    Create Solution.java

---
+++ Maximum Average Subarray 1/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int max=Integer.MIN_VALUE;
+        int p=0;
+        int sum=0;
+        for(int i=0; i<nums.length; i++){
+                sum+=nums[i];
+                if(i >= k-1){
+                    max= Math.max(max,sum);
+                    sum-= nums[p++];
+                }
+        }
+        return (double)max/k;
+    }
+}
